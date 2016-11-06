692dbdb85f22a3725882ab9c892eded72f29b041
Zion Koyl <hiszd1@gmail.com>
2016-11-06 18:41:23 -0500

    recommit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).on("load", function() {
+  alert("loaded");
+});
+$("#left").on("click", function() {
+  alert("Left");
+});
+$("#center").on("click", function() {
+  alert("Center");
+});
+$("#right").on("click", function() {
+  alert("Right");
+});
